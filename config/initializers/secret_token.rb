6adb72998359c632968fd9626887aaff9dd55ac0
Susan Holcomb <thefurtheroff@gmail.com>
2013-07-26 10:41:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MvpV1::Application.config.secret_token = '7be732603abec669852b9d40d6e57aa17f2cb6a323a7095a725a050bb5c14b6b60acf48623dd3fbf6c71071740e4f39cbe2efd3008dc916605f18fb588b1d5e4'
