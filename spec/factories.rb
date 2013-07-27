5341dbbd4b3868c4281fc077e74aac04f8d0c804
Susan Holcomb <thefurtheroff@gmail.com>
2013-07-27 00:04:32 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name     "Susan Holcomb"
+		email    "thefurtheroff@gmail.com"
+		password "Frankfurt08"
+		password_confirmation "Frankfurt08"
+	end	
+end
